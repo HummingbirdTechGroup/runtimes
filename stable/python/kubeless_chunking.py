1a7604c3ad5f36626e651d6be83e69a8098d7b34
Yuan Gao <yuan@hummingbirdtech.com>
2020-02-12 15:50:48 +0000

    increase MEMFILE_MAX to 100MB default

--- stable/python/kubeless_chunking.py
+++ stable/python/kubeless_chunking.py
@@ -22,6 +22,9 @@ timeout = float(os.getenv('FUNC_TIMEOUT', 180))
 pipe_chunk_size = int(os.getenv('FUNC_CHUNK_SIZE', 32768))
 chunk_min_wait = float(os.getenv('FUNC_CHUNK_MIN_WAIT', 0.1))
 
+memfile_max = int(os.getenv('FUNC_MEMFILE_MAX', 100*1024*1024))
+bottle.BaseRequest.MEMFILE_MAX = memfile_max
+
 app = application = bottle.app()
 
 func_hist = prom.Histogram('function_duration_seconds',
